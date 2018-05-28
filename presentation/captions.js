02a752c524c3500b2fe15a0ff89b6108ef7f9cba
IbraheemRodrigues <28712361+IbraheemR@users.noreply.github.com>
2018-05-28 13:27:47 +0100

    Fixed readability issues, and added ability to move with buttons

--- presentation/captions.js
+++ presentation/captions.js
@@ -1,4 +1,25 @@
+var hidden = false;
 var height;
+var id;
+
+//----------------------------------------------------
+
+function scrollTo(num) {
+  if (num < 1) {
+    num = 1;
+    return false;
+  } else if (num > $(".img").length) {
+    num = $(".img").length
+    return false;
+  }
+  var target = $("#img" + num);
+  $('html, body').animate({
+    scrollTop: target.offset().top
+  }, 500)
+  return true;
+}
+
+//----------------------------------------------------
 
 $(window).resize(function() {
   height = $(".img").height();
@@ -7,12 +28,35 @@ $(window).resize(function() {
 $(window).scroll(function() {
   var pos = $(window).scrollTop();
 
-  var id = Math.round( (pos)/height ) + 1; // Figureb out which image we are looking at
+  id = Math.round( pos/height ) + 1; // Figure out which image we are looking at
 
   $("#caption").html(
     $("#img" + id).html()
   );// Load associated caption
 });
+
+$(window).keydown(function(e) {
+  var k = e.keyCode;
+  if (k == 72) {//H key pressed, hide the caption bar
+    hidden = !hidden;
+    if (hidden) {
+      $("#caption").hide()
+    } else {
+      $("#caption").show()
+    }
+  } else if (k == 32 || k == 40) { //Space bar or down arrow, move to next slide
+    e.preventDefault();
+    id++; // This will put id out of syn with wher the window is, so the interval below will scroll us
+    scrollTo(id);
+  } else if (k == 38) { // Up arrow, move to previous slide
+    e.preventDefault();
+    id--;
+    scrollTo(id);
+  }
+});
+
+
+//----------------------------------------------------
 
 // Init
 $(window).resize();
@@ -23,3 +67,4 @@ $(".img").each(function() {
   var img = $(this).attr("image") || "https://picsum.photos/1200/800/?random";
   $(this).css("background-image", "url("+img+")");
 });
+
